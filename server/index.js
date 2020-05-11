1cf2f240a0aab59be0e96613cf63294c1e6ecc6d
leon <liyang@hosecloud.com>
2020-05-11 21:05:19 +0800

    lib

--- server/index.js
+++ server/index.js
@@ -6,7 +6,10 @@ const { renderToString } = require('react-dom/server')
 const SSR = require('../dist/search-server')
 const fs = require('fs')
 const path = require('path')
-const template = fs.readFileSync(path.join(__dirname, '../dist/search.html'), 'utf-8')
+const template = fs.readFileSync(
+  path.join(__dirname, '../dist/search.html'),
+  'utf-8'
+)
 const server = (port) => {
   const app = express()
   app.use(express.static('dist'))
@@ -25,18 +28,19 @@ const renderMarkuo = (str) => {
   const data = JSON.stringify(dataObj)
   // return `<!DOCTYPE html>
   // <html lang="en">
-  
+
   // <head>
   //   <meta charset="UTF-8">
   //   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   //   <title>Document</title>
   // </head>
-  
+
   // <body>
   //   <div id="root1">${str}</div>
   // </body>
-  
+
   // </html>`
-  return template.replace('<!--HTML_PLACEHOLDER-->', str)
-            .replace('<!--INITIAL_DATA_PLACEHOLDER-->', `<script>${data}</script>`)
+  return template
+    .replace('<!--HTML_PLACEHOLDER-->', str)
+    .replace('<!--INITIAL_DATA_PLACEHOLDER-->', `<script>${data}</script>`)
 }
